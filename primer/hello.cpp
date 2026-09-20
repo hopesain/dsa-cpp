9544d5178b388dddbc34bf997c6221a6cc459b32
hopesain <hopessain@gmail.com>
2026-09-20 21:46:05 +0200

    working with variables and std library

--- primer/hello.cpp
+++ primer/hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main() {
     int a = 1;
@@ -9,7 +10,25 @@ int main() {
     a = b;
     b = c;
     
+    std::string userName = "Hope Sain";
+    userName = "aa";
     std::cout << b;
-
+    std::cout << userName;
+    
+    // Character In
+    std::cout << "Enter a value: ";
+    int value;
+    std::cin >> value;
+    std::cout << "You entered: " << value;
     return 0;
+}
+
+void printValue() {
+    const double PI = 3.14;
+    std::cout << "PI Value: " << PI;
+}
+
+int increment(int initialValue) {
+    int value = initialValue++;
+    return value;
 }
